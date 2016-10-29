556baac0791a96c0f5021f96786f0562fac89bc9
Sergio <segova07@gmail.com>
2016-10-29 19:35:24 +0200

    Zorabot Update
    + Añadida opción de autopublicar la última noticia de tu web en un canal
    de discord.
    + Se ha aumentado el número de respuestas posibles que puede ofrecer el
    bot.
    + Se ha comentado parte del código para que sea más fácil de entender.
    - Se ha eliminado zorabot-min ya que no proporcionaba un aumento de
    rendimiento significativo.
    + Algunos bugs menores resueltos.

--- zorabot.js
+++ zorabot.js
@@ -17,9 +17,18 @@
 
 const Discord = require("discord.js");
 const pack = require('./package.json');
+
 const request = require('request');
 const Zorabot = new Discord.Client();
+
 const winston = require('winston');
+
+/*********************************************
+Página web de donde se obtiene la información.
+Debe tener el plugin WP REST API instalado.
+**********************************************/
+const WPAPI = require('wpapi');
+const apiPromise = WPAPI.discover('http://www.your_web.com');
 
 
 /***********  BIBLIOTECA DE RESPUESTAS ************/
@@ -34,7 +43,7 @@ var responseObject = {
 /**************************************************/
 
 Zorabot.on('message', function(message) {
-	if(message.author != "Zorabot") {
+	if(message.author !== Zorabot.user) {
 		if (responseObject[message.content.toLowerCase()]) {
 			message.reply(responseObject[message.content.toLowerCase()]);
 		}
@@ -55,10 +64,15 @@ Zorabot.on('message', function(message) {
 	}
 });
 
+setInterval(function() {
+	autoPublicar();
+}, 300000);
+
 /***********  BIBLIOTECA DE FUNCIONES  ***********/
 
 function elTiempo(mensaje) {
 	let loc = mensaje.content.substring(3);
+	let id = 'YOUR_OPENWEATHERMAP_ID';
 
 	if(loc == "") {
 		mensaje.channel.sendMessage('Por favor, ¡indica una localización válida! >_< (Ejemplo:  .w Madrid o .w Madrid,ES)').catch(error => console.log(error.stack));
@@ -66,12 +80,11 @@ function elTiempo(mensaje) {
 	else {
 		request(
 			{
-				url: 'http://api.openweathermap.org/data/2.5/weather?q='+loc+'&lang=es&appid=8efba9c322cd0ff864d0ad9708f4fb65',
+				url: 'http://api.openweathermap.org/data/2.5/weather?q='+loc+'&lang=es&appid='+id,
 				json: true
 			},
 			function (error, response, body) {
 				mensaje.channel.sendMessage(':: '+body["name"]+', '+body["sys"]["country"]+' :: '+body['weather'][0]['description']+' :: Temperatura: '+parseFloat(body["main"]["temp"]-273.15).toFixed(2)+'°C :: Presión: '+body["main"]["pressure"]+'mb :: Humedad: '+body["main"]["humidity"]+'%').catch(error => console.log(error.stack));
-				//console.log(body['weather']);
 			}
 		);
 	}
@@ -85,7 +98,8 @@ function animarFansubbers(mensaje) {
 		"¡marchando!", 
 		"¡oído cocina!",
 		"¡ahí que va!",
-		"¡marchando una de látigo!"
+		"¡marchando una de látigo!",
+		"¡a la orden!"
 	];
 	let respuesta2 = [
 		"¡¡¡CURRAD VAGOS DEL MAL!!! :<", 
@@ -95,8 +109,8 @@ function animarFansubbers(mensaje) {
 		"Me rindo... Pero dejo mi fotito latigando igualmente <3"
 	];
 	
-	mensaje.reply(respuesta1[Math.floor(Math.random() * respuesta1.length)]+"\n");
-	mensaje.channel.sendFile("/home/segova/Zorabot/img/latigo.jpg", "latigo.jpg", respuesta2[Math.floor(Math.random() * respuesta2.length)])
+	mensaje.reply(respuesta1[Math.floor(Math.random() * respuesta1.length)]);
+	mensaje.channel.sendFile("YOUR_IMG_PATH/IMG.JPG", "IMG.JPG", respuesta2[Math.floor(Math.random() * respuesta2.length)])
 }
 
 function zorabot(mensaje) {
@@ -107,7 +121,32 @@ function zorabot(mensaje) {
 		"¡Soy el ro-bot más avanzdo del mundo! :D", 
 		"Pues sí, he aquí mi figura... ¿A que molo?"
 	];
-	mensaje.channel.sendFile("/home/segova/Zorabot/img/Meika.png", "Meika.png", respuesta2[Math.floor(Math.random() * respuesta2.length)])
+	mensaje.channel.sendFile("YOUR_IMG_PATH/IMG2.JPG", "IMG2.JPG", respuesta2[Math.floor(Math.random() * respuesta2.length)])
+}
+
+function autoPublicar() {
+	let canal = 'YOUR_CHANNEL'; // Canal destino en el que se publicará.
+	let respuesta = [
+	"@everyone, ¡tenemos nueva release!",
+	"@everyone, ¡marchando release recién salida del horno!",
+	"@everyone, ¡moar dronja para vosotros! <3",
+	"@everyone, os dejo una cosita por aquí... De nada (?)",
+	"@everyone, ¡release fresquita para vuestro body! >__<",
+	"@everyone, marchando un episodio de algo que no puedo ver porque soy un bot... ;___;",
+	"@everyone, ¡¡¡fire in the hole!!! Os dejo un nuevo episodio por aquí :3",
+	"@everyone, me comentan los de arriba que publique esto y yo lo dejo por aquí como buen bot que soy :3",
+	"@everyone, los de arriba me explotan con tanto poner releases... (?) /me runs",
+	"@everyone, estamos on fire, así que aquí va una release calentita :)"
+	];
+	
+	apiPromise.then(function(site) {
+		site.posts().then(function(posts) {
+			if(Zorabot.channels.get(canal).messages.last()['content'].indexOf(posts[0]['link']) == -1) {
+				Zorabot.channels.get(canal).sendMessage(respuesta[Math.floor(Math.random() * respuesta.length)]);
+				Zorabot.channels.get(canal).sendMessage(posts[0]['link']);
+			}
+		}); 
+	});
 }
 
 /**************************************************/
